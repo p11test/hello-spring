ddd7fb5ae04b4e4cc28e2238941517c467f943cd
jsyang@inznet.com <jsyang@inznet.com>
2021-12-06 10:19:20 +0900

    컴포넌트 스캔과 자동 의존 관계  설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,16 +3,20 @@ package hello.hellospring.service;
 import hello.hellospring.Repository.MemberRepository;
 import hello.hellospring.Repository.MemoryMemberRepository;
 import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
 
 //    회원 리포지토리의 코드가 회원 서비스 코드를 DI 가능하게 변경한다.
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
